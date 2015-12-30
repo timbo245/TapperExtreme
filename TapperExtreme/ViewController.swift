580451aadd0de62f4e1ea5f6530f0959dd41ff24
Timothy Lott <timbo245@yahoo.com>
2015-12-29 22:09:39 -0800

    Updated and completed app

--- TapperExtreme/ViewController.swift
+++ TapperExtreme/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+// Properties
+    
+    var maxTaps: Int = 0
+    var currentTaps: Int = 0
+    
+    func updateTapsLbl() {
+        tapsLbl.text = "\(currentTaps) Taps "
+    }
 
+// Outlets
     @IBOutlet weak var logoImg: UIImageView!
     @IBOutlet weak var howManyTapsTxt: UITextField!
     @IBOutlet weak var playBtn: UIButton!
@@ -17,14 +27,55 @@ class ViewController: UIViewController {
     @IBOutlet weak var tapBtn: UIButton!
     @IBOutlet weak var tapsLbl: UILabel!
 
-    @IBAction func onPlayBtnPressed(sender: UIButton!) {
-        logoImg.hidden = true
-        playBtn.hidden = true
-        howManyTapsTxt.hidden = true
+    @IBAction func onCoinTapped(sender: UIButton!) {
+        currentTaps++
+        updateTapsLbl()
         
-        tapBtn.hidden = false
-        tapsLbl.hidden = false
+        if isGameOver() {
+            restartGame()
+        }
+        
     }
-
+    
+    @IBAction func onPlayBtnPressed(sender: UIButton!) {
+        
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != "" {
+            
+            logoImg.hidden = true
+            playBtn.hidden = true
+            howManyTapsTxt.hidden = true
+            
+            tapBtn.hidden = false
+            tapsLbl.hidden = false
+            
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            updateTapsLbl()
+            
+        }
+    }
+    
+    func restartGame() {
+        maxTaps = 0
+        howManyTapsTxt.text = ""
+        
+        logoImg.hidden = false
+        playBtn.hidden = false
+        howManyTapsTxt.hidden  = false
+        
+        tapBtn.hidden = true
+        tapsLbl.hidden = true
+        
+        
+    }
+    
+    func isGameOver() -> Bool {
+        if currentTaps >= maxTaps {
+            return true
+        }else{
+            return false
+        }
+    }
 }
+
 
